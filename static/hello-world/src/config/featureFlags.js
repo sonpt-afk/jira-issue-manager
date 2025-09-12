30683080d3ccbcf5d6e146e7bc293c635798718d
kypro34 <sonbappe89@gmail.com>
2025-09-12 23:07:18 +0700

    refactor: update manifest to use react-app resource and add permissions for Jira scopes + add feature flags configuration for issue manager features

---
+++ static/hello-world/src/config/featureFlags.js
@@ -0,0 +1,7 @@
+import { FlagConfig } from "@atlaskit/feature-flags";
+
+export const defaultFlags = {
+  "jira.issue-manager.features.project-selection": true,
+  "jira.issue-manager.features.dynamic-table": true,
+  "jira.issue-manager.features.pagination": true,
+};
